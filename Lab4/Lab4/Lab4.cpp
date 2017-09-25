733562a779d7d9f14dbd25e114a241ba7570939e
Matthew <kavanamw@mail.uc.edu>
2017-09-25 15:59:46 -0400

    Made some changes to lab 4, got a little better

--- Lab4/Lab4/Lab4.cpp
+++ Lab4/Lab4/Lab4.cpp
@@ -11,13 +11,14 @@ protected:
 	X *array[MaxElm];
 	int idx;
 public:
-	//initilizes the values to 0
-	orderedlist()
+	//initilizes the values to -9999
+	void listTo0()
 	{
 		idx = 0;
 		for (int x = 0; x < MaxElm; x++)
 		{
-			array[x] = 0;
+			array[x] = new X;
+			*array[x] = -9999;
 		}
 	}
 	//adds and sorts an item to the array of pointers
@@ -25,23 +26,22 @@ public:
 	{
 		int temp = 0;
 		//sets first element to the begining of the array
+		//seems to work as intended
 		if (idx == 0)
 		{
-			array[0] = new X;
 			*array[0] = TempInput;
 			idx++;
 			return;
 		}
-		for (int t = 0; t < idx; t++)
+		//sort array
+		for (int t = 1; t < MaxElm; t++)
 		{
 			//if element before is larger than current input
 			if (*array[t] > TempInput)
 			{
-
 				///*
 				temp = *array[t];
 				*array[t] = TempInput;
-				array[t + 1] = new X;
 				*array[t + 1] = temp;
 				idx++;
 				return;
@@ -50,10 +50,8 @@ public:
 			//if element before is larger than current input
 			else
 			{
-
 				///*
-				array[t] = new X;
-				*array[t] = TempInput;
+				*array[t + 1] = TempInput;
 				idx++;
 				return;
 				//*/
@@ -63,10 +61,9 @@ public:
 	//functon to print the entire array begining to end after addition and sorting. 
 	void Print()
 	{
-		for (int j = 0; j < idx; j++)
+		for (int j = 0; j < MaxElm; j++)
 		{
-			//cout << MaxElm << endl;
-
+			//cout << "Here" << endl;
 			cout << *array[j] << endl;
 		}
 	}
@@ -86,6 +83,8 @@ public:
 				return false
 		}
 	}
+
+	/*
 	bool IsFull()
 	{
 
@@ -94,18 +93,22 @@ public:
 	{
 
 	}
+	*/
 };
 
 int main()
 {
 	orderedlist<int> L4;
 	//L4.IsEmpty();
-	L4.AddItem(2);
+	L4.listTo0();
+	L4.AddItem(18);
 	L4.AddItem(1);
-	L4.AddItem(15);
+	L4.AddItem(2);
+	/*
 	L4.AddItem(10);
 	L4.AddItem(7);
 	L4.AddItem(9);
+	*/	
 	L4.Print();
 	cin.get();
 	return 0;
